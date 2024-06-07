ee9a1f8236a4fc98947e9e8e332dc2452f9e2c03
Nick Valente <nick.valente@oakknoll.org>
2024-06-07 17:08:01 -0400

    Modified .config/fish, .config/powershell

--- .config/fish/configtools.fish
+++ .config/fish/configtools.fish
@@ -4,12 +4,32 @@
 #                                                                              #
 ################################################################################
 
-# Set .rig and .dev directories
+# Set environment variables
 set RIG "$HOME/.rig"
 set DEV "$HOME/.dev"
 
-# General
+# Dotfile rigging
 alias rig="git --git-dir=$RIG --work-tree=$HOME"
+
+function rig-up
+    for arg in $argv
+        rig add $HOME/$arg
+        echo "Added $arg"
+    end
+    set commitMessage "Modified "(string join ", " $argv)
+    rig commit -m "$commitMessage"
+end
+
+function rig-down
+    rig reset --hard HEAD
+    rig pull
+end
+
+
+# GitHub Copilot
+alias suggest="gh copilot suggest"
+alias explain="gh copilot explain"
+
 
 # Arch Linux / Pacman
 alias pacplay="sudo pacman -Syu"
