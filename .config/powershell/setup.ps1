243f3d4ef81d82fa9af7349f24a6d14ed913a9bb
Nick Valente <nick.valente@oakknoll.org>
2024-06-08 00:08:35 -0400

    Modified README.md, .config/powershell

--- .config/powershell/setup.ps1
+++ .config/powershell/setup.ps1
@@ -8,8 +8,12 @@ $env:XDG_CACHE_HOME = "$HOME\.cache"
 [Environment]::SetEnvironmentVariable('XDG_DATA_HOME', $env:XDG_DATA_HOME, 'User')
 [Environment]::SetEnvironmentVariable('XDG_CACHE_HOME', $env:XDG_CACHE_HOME, 'User')
 
-# Direct PowerShell $PROFILE to load the configuration file 
-'. "$env:XDG_CONFIG_HOME\powershell\config.ps1"' | Out-File -Append -FilePath $PROFILE
+# Direct PowerShell $PROFILE to launch the configuration file 
+$launchcode = '. "$env:XDG_CONFIG_HOME\powershell\config.ps1"'
+# If $PROFILE does not contain launch code, append it
+if ((Get-Content $PROFILE) -notcontains $launchcode) {
+    $launchcode | Out-File -Append -FilePath $PROFILE
+}
 
 # Restart the shell
 . $PROFILE
