5106816a5fbe66ad56fc10a9d24154a6da73ec9f
Nick Valente <nick.valente@oakknoll.org>
2024-08-30 09:46:10 -0400

    Modified .config/powershell

--- .config/powershell/configtools.ps1
+++ .config/powershell/configtools.ps1
@@ -17,19 +17,24 @@ function reload {
 
 
 # Config rig functions
+
 function rig { 
+    # Run git command in rig mode
     git --git-dir=$RIG_HOME --work-tree=$HOME $args 
 }
 
 function rig-reset {
+    # Resets the current branch to the state of the latest commit, discarding any changes made after that commit.
 	rig reset --hard HEAD
 }
 
 function rig-list {
+    # List all files in the rig repository
     rig ls-tree -r HEAD --name-only
 }
 
 function rig-up {
+    # Add files to the rig repository
 	foreach ($arg in $args) {
     	rig add $HOME\$arg
 		echo "Added $arg"
@@ -40,13 +45,15 @@ function rig-up {
 }
 
 function rig-down {
+    # Pull changes from the rig repository
 	rig reset --hard HEAD
 	rig pull
 }
 
 function rig-remove {
+    # Remove files from the rig repository
     foreach ($arg in $args) {
-        rig rm --cached $HOME\$arg
+        rig rm -r --cached $HOME\$arg
         echo "Removed $arg"
     }
     $commitMessage = "Removed " + ($args -join ", ")
@@ -55,6 +62,7 @@ function rig-remove {
 }
 
 function rig-ignore {
+    # Add files to the .gitignore file
     foreach ($arg in $args) {
         echo $arg | Out-File -Append -Encoding utf8 $HOME\.gitignore
         echo "Ignored $arg"
@@ -63,7 +71,6 @@ function rig-ignore {
     rig commit -m "Updated .gitignore"
     rig push
 }
-
 
 # Github Copilot
 function suggest {
