f8c2a3128da55dde776a4340f4ca60cb04de2381
Nick Valente <nick.valente@oakknoll.org>
2024-12-26 16:48:23 -0500

    Modified .config/fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,8 +11,10 @@ set fish_greeting "Go fish !!"
 # Load toolbox
 source $XDG_CONFIG_HOME/fish/configtools.fish
 
-# Load Homebrew shell environment
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+# Launch Homebrew shell environment
+if type -q ~/linuxbrew/.linuxbrew/bin/brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
 
 # Launch Starship
 if type -q starship
