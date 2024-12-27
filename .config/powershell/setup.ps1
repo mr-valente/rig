80fc6dd0a037c195f03543fc2f3c9a61ececf463
Nick Valente <nick.valente@oakknoll.org>
2024-12-27 15:31:30 -0500

    Modified .config/fish, .config/powershell

--- .config/powershell/setup.ps1
+++ .config/powershell/setup.ps1
@@ -1,11 +1,9 @@
 # Define XDG Base Directory variables
-$env:HOME = $HOME
 $env:XDG_CONFIG_HOME = "$HOME\.config"
 $env:XDG_DATA_HOME = "$HOME\.local\share"
 $env:XDG_CACHE_HOME = "$HOME\.cache"
 
 # Set the XDG Base Directory variables in the environment
-[Environment]::SetEnvironmentVariable('HOME', $env:HOME, 'User')
 [Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', $env:XDG_CONFIG_HOME, 'User')
 [Environment]::SetEnvironmentVariable('XDG_DATA_HOME', $env:XDG_DATA_HOME, 'User')
 [Environment]::SetEnvironmentVariable('XDG_CACHE_HOME', $env:XDG_CACHE_HOME, 'User')
@@ -20,3 +18,8 @@ if ((Get-Content $PROFILE) -notcontains $launchcode) {
 
 # Restart the shell
 . $PROFILE
+
+# Set up .gitconfig
+rig config --global core.autocrlf false
+rig config --global push.autoSetupRemote true
+rig config --global status.showUntrackedFiles no
