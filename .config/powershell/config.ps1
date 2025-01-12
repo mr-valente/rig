305097356081e85d1c695bb719c0cf44d4f988c7
Nick Valente <valente.math@gmail.com>
2025-01-12 16:23:53 -0500

    Modified .config/powershell

--- .config/powershell/config.ps1
+++ .config/powershell/config.ps1
@@ -40,16 +40,3 @@ $POWERTOOLS = "$env:XDG_CONFIG_HOME\powershell\powertools"
 
 # Bitwarden
 . "$POWERTOOLS\bitwarden.ps1"
-
-
-
-
-# # Load toolbox
-# . "$env:XDG_CONFIG_HOME\powershell\configtools.ps1"
-
-# # Check if Starship exists
-# # if (Get-Command starship -ErrorAction SilentlyContinue) {
-# if (Command-Exists "starship") {
-#     # Launch Starship
-#     Invoke-Expression (&starship init powershell)
-# }
