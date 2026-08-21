d8771f671a1c4035c0eb98a7c1f6349faa422ae3
Nicholas Valente <valente.math@gmail.com>
2026-08-20 20:55:49 -0400

    Modified .config/fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,29 +15,5 @@ for f in $TACKLEBOX/*.fish
     source $f
 end
 
-# (temporary) Disable Config Rig status prompt
 rig-status --quiet
 
-###############################################################################
-# (temporary) Function to check Arch mainline kernel version
-
-function check_mainline --description "Check if Arch mainline kernel is 7.0+"
-    # Fetch the version
-    set -l current_mainline (curl -s "https://archlinux.org/packages/search/json/?name=linux" | jq -r ".results[0].pkgver")
-    
-    # Check if version is 7.0 or higher
-    # Regex explanation:
-    # ^           = Start of string
-    # ([7-9]|     = Major versions 7, 8, 9
-    # [1-9][0-9]) = Major versions 10+
-    # \.          = Literal dot before minor version
-    if string match -qr "^([7-9]|[1-9][0-9])\." -- $current_mainline
-        set_color -o green
-        echo ""
-        echo "🚨 ALERT: Linux mainline has updated to $current_mainline!"
-        echo ""
-        set_color normal
-    end
-end
-
-# check_mainline
---
+++ .config/fish/tacklebox/pacman.fish
@@ -0,0 +1,7 @@
+alias zazen="sudo pacman -Syu"
+
+alias hello="sudo pacman -S --noconfirm"
+alias goodbye="sudo pacman -Rns --noconfirm"
+alias list="pacman -Qe"
+# alias packey="pacman -Sy archlinux-keyring && pacman -Su"
+
